833dfb03081141b734838d79e220e4e874da4a6a
user <user@SANDEEP-PC>
2021-12-25 16:32:20 +0530

    First commit

---
+++ learn-spring-boot/src/main/java/com/learn/learnspringboot/courses/controller/CourseController.java
@@ -0,0 +1,35 @@
+package com.learn.learnspringboot.courses.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learn.learnspringboot.courses.bean.Course;
+
+@RestController
+public class CourseController {
+	
+	  // http://localhost:8080/courses
+	@GetMapping("/courses")
+	public List<Course> getAllCourses(){
+		
+		return List.of(new Course(1,"Learn Microservice","in30mins"), new Course(2,"Learn Cloud","in 6 days"));
+	}
+	
+	// http://localhost:8080/courses/1
+	
+	@GetMapping("/courses/3")
+	public Course getCourseDetails() {
+		return new Course(3,"Learn Angular JS","in60mins");
+		
+	}
+	
+	@GetMapping("/courses/4")
+	public Course getCourseDetail() {
+		return new Course(4,"Learn React JS","in45mins");
+		
+	}
+
+
+}
